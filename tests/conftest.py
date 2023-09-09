564ecc5a55bfb240ab2c1fb7f17bc4c69dafefae
Andreas Motl <andreas.motl@crate.io>
2023-09-10 00:49:21 +0200

    Tests: Provide fixture `testdrive_engine` for the whole test suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,15 @@
+import mlflow
+import pytest
+
 from mlflow_cratedb import patch_all
 
 patch_all()
+
+
+# The canonical database schema used for test purposes is `testdrive`.
+DB_URI = "crate://crate@localhost/?schema=testdrive"
+
+
+@pytest.fixture
+def testdrive_engine():
+    yield mlflow.store.db.utils.create_sqlalchemy_engine_with_retry(DB_URI)
