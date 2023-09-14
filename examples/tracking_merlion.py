2973a61c5cd9272dceb094d9713d0739d58cfafb
Niklas Schmidtmer <niklas@crate.io>
2023-09-14 08:36:21 +0200

    Retrieve all data from CrateDB/persist detected anomalies
    Co-authored-by: Andreas Motl <andreas.motl@crate.io>

--- examples/tracking_merlion.py
+++ examples/tracking_merlion.py
@@ -28,6 +28,7 @@ Resources
 - https://github.com/numenta/NAB
 """
 
+import datetime as dt
 import os
 
 import mlflow
@@ -64,7 +65,7 @@ def table_exists(table_name: str, schema_name: str = "doc") -> bool:
     return rowcount > 0
 
 
-def import_data(table_name: str):
+def import_data(data_table_name: str, anomalies_table_name: str):
     """
     Download Numenta Anomaly Benchmark data, and load into database.
     """
@@ -81,11 +82,34 @@ def import_data(table_name: str):
     with connect_database() as conn:
         cursor = conn.cursor()
         # Create the table if it doesn't exist.
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (timestamp TIMESTAMP, temperature DOUBLE)")
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {data_table_name} (timestamp TIMESTAMP, temperature DOUBLE)")
         # Insert the data in chunks.
         for chunk in chunks:
-            sql = f"INSERT INTO {table_name} (timestamp, temperature) VALUES (?, ?)"  # noqa: S608
+            sql = f"INSERT INTO {data_table_name} (timestamp, temperature) VALUES (?, ?)"  # noqa: S608
             cursor.executemany(sql, list(chunk.itertuples(index=False, name=None)))
+
+        # Create the table to hold known and detected anomalies.
+        cursor.execute(
+            f"""
+                CREATE TABLE IF NOT EXISTS {anomalies_table_name} (
+                    "ts_start" TIMESTAMP WITHOUT TIME ZONE,
+                    "ts_end" TIMESTAMP WITHOUT TIME ZONE,
+                    "score" DOUBLE PRECISION,
+                    "experiment_id" BIGINT,
+                    "run_id" TEXT,
+                    "description" TEXT
+                );
+            """
+        )
+
+        known_anomalies = [
+            ("2013-12-15 17:50:00.000000", "2013-12-17 17:00:00.000000"),
+            ("2014-01-27 14:20:00.000000", "2014-01-29 13:30:00.000000"),
+            ("2014-02-07 14:55:00.000000", "2014-02-09 14:05:00.000000"),
+        ]
+        cursor.executemany(
+            f"INSERT INTO {anomalies_table_name} (ts_start, ts_end) VALUES (?, ?)", known_anomalies  # noqa: S608
+        )
 
 
 def read_data(table_name: str) -> pd.DataFrame:
@@ -96,12 +120,12 @@ def read_data(table_name: str) -> pd.DataFrame:
     with conn:
         cursor = conn.cursor()
         cursor.execute(
-            f"""SELECT
-         DATE_BIN('5 min'::INTERVAL, "timestamp", 0) AS timestamp,
-         MAX(temperature) AS value
-         FROM {table_name}
-         GROUP BY timestamp
-         ORDER BY timestamp ASC"""
+            f"""SELECT DATE_BIN('5 min'::INTERVAL, "timestamp", 0),
+                       MAX(temperature)
+                FROM {table_name}
+                GROUP BY 1
+                ORDER BY 1 ASC
+            """  # noqa: S608
         )
         data = cursor.fetchall()
 
@@ -113,7 +137,48 @@ def read_data(table_name: str) -> pd.DataFrame:
     return time_series.set_index("timestamp")
 
 
-def run_experiment(time_series: pd.DataFrame):
+def read_anomalies():
+    """
+    Read anomalies from database into pandas DataFrame.
+    """
+    conn = connect_database()
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT ts_start, ts_end FROM anomalies WHERE experiment_id IS NULL")
+        data = cursor.fetchall()
+        return [[pd.to_datetime(start, unit="ms"), pd.to_datetime(end, unit="ms")] for start, end in data]
+
+
+def save_anomalies(table_name: str, anomalies: pd.DataFrame, mttd: int):
+    """
+    Save anomalies from a pandas DataFrame into the database.
+    """
+    conn = connect_database()
+    with conn:
+        cursor = conn.cursor()
+        rows = []
+        for index, row in anomalies.iterrows():
+            rows.append(
+                (
+                    index,
+                    index + dt.timedelta(seconds=mttd),
+                    row["anom_score"],
+                    row["experiment_id"],
+                    row["run_id"],
+                    None,
+                )
+            )
+
+        cursor.executemany(
+            f"""
+                INSERT INTO {table_name} (ts_start, ts_end, score, experiment_id, run_id, description)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """,  # noqa: S608
+            rows,
+        )
+
+
+def run_experiment(time_series: pd.DataFrame, anomalies_table_name: str):
     """
     Run experiment on DataFrame, using Merlion. Track it using MLflow.
     """
@@ -131,13 +196,8 @@ def run_experiment(time_series: pd.DataFrame):
         test_pred = model.get_anomaly_label(time_series=test_data)
 
         # Prepare the test labels
-        time_frames = [
-            ["2013-12-15 17:50:00.000000", "2013-12-17 17:00:00.000000"],
-            ["2014-01-27 14:20:00.000000", "2014-01-29 13:30:00.000000"],
-            ["2014-02-07 14:55:00.000000", "2014-02-09 14:05:00.000000"],
-        ]
+        time_frames = read_anomalies()
 
-        time_frames = [[pd.to_datetime(start), pd.to_datetime(end)] for start, end in time_frames]
         time_series["test_labels"] = 0
         for start, end in time_frames:
             time_series.loc[(time_series.index >= start) & (time_series.index <= end), "test_labels"] = 1
@@ -165,6 +225,14 @@ def run_experiment(time_series: pd.DataFrame):
         model.save("model")
         mlflow.log_artifact("model")
 
+        anomalies = test_pred.to_pd()
+        anomalies = anomalies[anomalies["anom_score"] > 0]
+        anomalies["experiment_id"] = current_run.info.experiment_id
+        anomalies["run_id"] = current_run.info.run_id
+
+        # Save detected anomalies to CrateDB
+        save_anomalies(anomalies_table_name, anomalies, mttd.total_seconds())
+
 
 def main():
     """
@@ -174,15 +242,18 @@ def main():
     # Table name where the actual data is stored.
     data_table = "machine_data"
 
+    # Taable name where to store anomalies
+    anomalies_table = "anomalies"
+
     # Provision data to operate on, only once.
     if not table_exists(data_table):
-        import_data(data_table)
+        import_data(data_table, anomalies_table)
 
     # Read data into pandas DataFrame.
     data = read_data(data_table)
 
     # Run experiment on data.
-    run_experiment(data)
+    run_experiment(data, anomalies_table)
 
 
 if __name__ == "__main__":
