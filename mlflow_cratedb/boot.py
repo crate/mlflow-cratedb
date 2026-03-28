7d8f3eef1879fd73a6a62998b1a5cf2fe7196d4e
Andreas Motl <andreas.motl@crate.io>
2026-03-28 22:24:08 +0100

    Amalgamation: Make sure SQLAlchemy is patched first
    ... before importing any other modules.

--- mlflow_cratedb/boot.py
+++ mlflow_cratedb/boot.py
@@ -1,9 +1,5 @@
 import logging
 
-from mlflow_cratedb.patch.mlflow import patch_mlflow
-from mlflow_cratedb.patch.pycaret import patch_pycaret
-from mlflow_cratedb.patch.sqlalchemy import patch_sqlalchemy
-
 logger = logging.getLogger("mlflow")
 
 ANSI_YELLOW = "\033[93m"
@@ -14,6 +10,13 @@ def patch_all():
     logger.info(f"{ANSI_YELLOW}Amalgamating MLflow for CrateDB{ANSI_RESET}")
     logger.debug("To undo that, run `pip uninstall mlflow-cratedb`")
 
+    # Make sure SQLAlchemy is patched first, before importing any other modules.
+    from mlflow_cratedb.patch.sqlalchemy import patch_sqlalchemy
+
     patch_sqlalchemy()
+
+    from mlflow_cratedb.patch.mlflow import patch_mlflow
+    from mlflow_cratedb.patch.pycaret import patch_pycaret
+
     patch_mlflow()
     patch_pycaret()
