6369ba63566edc95088bc4645caafa18c73de7b3
Andreas Motl <andreas.motl@crate.io>
2026-04-24 04:28:06 +0200

    Tests: Improve efficiency of `reset_database` fixture
    Don't drop and re-create all tables each time,
    but truncate tables instead.

--- mlflow_cratedb/adapter/setup_db.py
+++ mlflow_cratedb/adapter/setup_db.py
@@ -1,4 +1,5 @@
 import importlib.resources
+from typing import cast
 
 import sqlalchemy as sa
 import sqlparse
@@ -22,6 +23,7 @@ def _setup_db_create_tables(engine: sa.Engine):
     with engine.connect() as connection:
         for statement in sqlparse.split(sql_statements):
             connection.execute(sa.text(statement))
+        connection.commit()
 
 
 def _setup_db_drop_tables(engine: sa.Engine):
@@ -32,3 +34,34 @@ def _setup_db_drop_tables(engine: sa.Engine):
     with engine.connect() as connection:
         for statement in sqlparse.split(sql_statements):
             connection.execute(sa.text(statement))
+        connection.commit()
+
+
+def _get_schema(engine: sa.Engine) -> str:
+    """Get schema from SQLAlchemy connection URL."""
+    schema = cast(str, engine.url.query.get("schema"))
+    if not schema:
+        raise ValueError("No schema specified")
+    if schema == "doc":
+        raise ValueError("Schema is `doc`. Will not operate on that.")
+    return schema
+
+
+def _setup_db_drop_schema(engine: sa.Engine):
+    """Drop all relevant database tables by dropping schema. Handle with care."""
+    schema = _get_schema(engine)
+    with engine.connect() as connection:
+        connection.execute(sa.text(f'DROP SCHEMA IF EXISTS "{schema}" CASCADE'))  # noqa: S608
+        connection.commit()
+
+
+def _setup_db_truncate_tables(engine: sa.Engine):
+    """Delete data from all relevant database tables. Handle with care."""
+    schema = _get_schema(engine)
+    with engine.connect() as connection:
+        inspector = sa.inspect(engine)
+        all_tables = inspector.get_table_names(schema=schema)
+        for table in all_tables:
+            connection.execute(sa.text(f'DELETE FROM "{table}"'))  # noqa: S608
+            connection.execute(sa.text(f'REFRESH TABLE "{table}"'))  # noqa: S608
+        connection.commit()
